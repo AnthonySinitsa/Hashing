ed09a847c0cfe9a5b6e48f9c78227e2da25f6161
Anton <itonyemail@gmail.com>
2024-01-09 14:21:17 -0800

    typo fix

--- Assets/Scripts/SmallXXHash.cs
+++ Assets/Scripts/SmallXXHash.cs
@@ -5,12 +5,13 @@ public struct SmallXXHash {
 	const uint primeC = 0b11000010101100101010111000111101;
 	const uint primeD = 0b00100111110101001110101100101111;
 	const uint primeE = 0b00010110010101100110011110110001;
+
+    
+    uint accumulator;
+
+    public SmallXXHash(int seed){
+        accumulator = (uint)seed + primeE;
+    }
+
+    public static implicit operator uint (SmallXXHash hash) => hash.accumulator;
 }
-
-uint accumulator;
-
-public SmallXXHash(int seed){
-    accumulator = (uint)seed + primeE;
-}
-
-public static implicit operator uint (SmallXXHash hash) => hash.accumulator;
