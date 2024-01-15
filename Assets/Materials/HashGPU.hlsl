ca337c5b8bc68441f798927e910adbed1943e8d9
Anton <itonyemail@gmail.com>
2024-01-15 15:57:18 -0800

    Update HashGPU.hlsl

--- Assets/Materials/HashGPU.hlsl
+++ Assets/Materials/HashGPU.hlsl
@@ -7,7 +7,6 @@ float4 _Config;
 
 void ConfigureProcedural () {
 	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-		
 		unity_ObjectToWorld = 0.0;
 		unity_ObjectToWorld._m03_m13_m23_m33 = float4(
 			_Positions[unity_InstanceID],
