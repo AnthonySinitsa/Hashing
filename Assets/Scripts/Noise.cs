24f72073229f92df1f78fee4718a26506c727160
Anton <itonyemail@gmail.com>
2024-01-18 16:42:14 -0800

    generic noise job

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -12,5 +12,35 @@ public static class Noise {
 	}
 
 	[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
-	public struct Job<N> : IJobFor where N : struct, INoise {}
+	public struct Job<N> : IJobFor where N : struct, INoise {
+
+        [ReadOnly]
+		public NativeArray<float3x4> positions;
+
+		[WriteOnly]
+		public NativeArray<float4> noise;
+
+		public SmallXXHash4 hash;
+
+		public float3x4 domainTRS;
+
+		public void Execute (int i) {
+			noise[i] = default(N).GetNoise4(
+				domainTRS.TransformVectors(transpose(positions[i])), hash
+			);
+		}
+        public static JobHandle ScheduleParallel (
+			NativeArray<float3x4> positions, NativeArray<float4> noise,
+			int seed, SpaceTRS domainTRS, int resolution, JobHandle dependency
+		) => new Job<N> {
+			positions = positions,
+			noise = noise,
+			hash = SmallXXHash.Seed(seed),
+			domainTRS = domainTRS.Matrix,
+		}.ScheduleParallel(positions.Length, resolution, dependency);
+    }
+    public delegate JobHandle ScheduleDelegate (
+		NativeArray<float3x4> positions, NativeArray<float4> noise,
+		int seed, SpaceTRS domainTRS, int resolution, JobHandle dependency
+	);
 }
